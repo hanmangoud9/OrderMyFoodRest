0e457287e4a05efcafb3f204652ba56091269a2f
hanmangoud9 <hanman2goud@yahoo.com>
2022-03-07 16:08:44 +0530

    Update OrderMyFoodApplication.java

--- OrderMyFood/src/main/java/com/tfpl/ordermyfood/OrderMyFoodApplication.java
+++ OrderMyFood/src/main/java/com/tfpl/ordermyfood/OrderMyFoodApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class OrderMyFoodApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Test");
 		SpringApplication.run(OrderMyFoodApplication.class, args);
 	}
 
